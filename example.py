a311f9dadf222a9679425a61bc2f868a9d44a410
MaksM <m.maksimov21@yandex.ru>
2021-11-10 08:43:49 +0300

    init commit

---
+++ example.py
@@ -0,0 +1,14 @@
+one = input()
+two = input()
+three = input()
+four = input()
+name = input()
+
+
+print(f"{four} - {name}")
+print(f"{three} - {name}")
+print(f"{two} - {name}")
+print(f"{one} - {name}")
+
+
+# "I wanna some '[drink]', my favorite meal - '[meal]'. Give me also '[subMeal]'. I will come soon... '[time]'
---
+++ lec2/example_a.py
@@ -0,0 +1,3 @@
+print("Hello, Bob!") #вывод строки в консоль
+print("I am 40")
+print("I'am from Moscow")
